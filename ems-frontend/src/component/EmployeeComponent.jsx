937e229d747aab6e5185a4e59dc141d56729ba2f
Huynh Thi Hong Thanh <thanh.huynh.9111994@gmail.com>
2025-02-23 00:00:20 +0700

    Change Add and Update Employee Feature to use Department - React App

--- ems-frontend/src/component/EmployeeComponent.jsx
+++ ems-frontend/src/component/EmployeeComponent.jsx
@@ -1,12 +1,24 @@
 import React, {useState, useEffect} from 'react'
 import { createEmployee, getEmployee, updateEmployee } from '../service/EmployeeService'
 import { useNavigate, useParams } from 'react-router-dom';
+import { getAllDepartments } from '../service/DepartmentService';
 
 const EmployeeComponent = () => {
 
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
+  const [departmentId, setDepartmentId] = useState('')
+  const [departments, setDepartments] = useState([])
+
+  // Many To One Relationship Spring Boot
+  useEffect(() => {
+    getAllDepartments().then((response) => {
+      setDepartments(response.data);
+    }).catch(error => {
+      console.error(error);
+    })
+  }, [])
 
   // Page Title
   const {id} = useParams();
@@ -15,7 +27,8 @@ const EmployeeComponent = () => {
   const [errors, setErrors] = useState( {
     firstName: '',
     lastName: '',
-    email: ''
+    email: '',
+    department: ''
   })
 
   // Navigation
@@ -28,6 +41,7 @@ const EmployeeComponent = () => {
         setFirstName(response.data.firstName);
         setLastName(response.data.lastName);
         setEmail(response.data.email);
+        setDepartmentId(response.data.departmentId);
       }).catch(error => {
         console.error(error);
       })
@@ -38,21 +52,17 @@ const EmployeeComponent = () => {
     e.preventDefault();
 
     if (validateForm()) {
-      if (id) {
-        const employee = {firstName, lastName, email}
-        console.log(employee)
+      const employee = {firstName, lastName, email, departmentId}
+      console.log(employee)
 
+      if (id) {
         updateEmployee(id, employee).then((response) => {
           console.log(response.data);
           navigator('/employees');
       }).catch(error => {
         console.error(error);
       })  
-      
     } else {
-      const employee = {firstName, lastName, email}
-      console.log(employee);
-  
       createEmployee(employee).then((response) => {
         console.log(response.data);
         navigator('/employees');
@@ -89,6 +99,13 @@ const EmployeeComponent = () => {
       valid = false;
     }
 
+    if (departmentId) {
+      errorsCopy.department = '';
+    } else {
+      errorsCopy.department = 'Department is required';
+      valid = false;
+    }
+
     setErrors(errorsCopy);
 
     return valid;
@@ -98,7 +115,7 @@ const EmployeeComponent = () => {
     if(id) {
       return <h2 className='text-center'>Update Employee</h2>
     } else {
-      <h2 className='text-center'>Add Employee</h2>
+      return <h2 className='text-center'>Add Employee</h2>
     }
   }
 
@@ -148,6 +165,25 @@ const EmployeeComponent = () => {
                 onChange={(e) => setEmail(e.target.value)}>
                 </input>
                 { errors.firstName && <div className='invalid-feedback'> { errors.email}</div>}
+              </div>
+
+              <div className='form-group mb-2'>
+                <label className='form-label'>Select Department:</label>
+
+              <select 
+              className={`form-control ${errors.department ? 'is-invalid': ''}`} 
+              value={departmentId} 
+              onChange={(e) => setDepartmentId(e.target.value)}>
+
+                <option value="Select Department">Select Department</option>
+                {
+                  departments.map( department => 
+                    <option key={department.id} value={department.id}>{department.departmentName}</option>
+                  )
+                }
+              </select>
+              { errors.department && <div className='invalid-feedback'> { errors.department}
+                </div>}
               </div>
 
               <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
